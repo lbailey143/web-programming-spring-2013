4134016f5e492230fcf12be6b50d26a9db63952b
unknown <lbailey@AMS-9-DC5700.tsra.org>
2013-01-31 11:34:16 -0800

    HTML cleanup

--- lesson-02/index.php
+++ lesson-02/index.php
@@ -1,16 +1,22 @@
 <!doctype html>
 <html>
+
 <head>
 <meta charset="utf-8">
 <title>Advanced Web Programming with PHP &amp; MySQL</title>
-
 <link href="css_lrb.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
+<!-- begin .container -->
 <div class="container">
-  <div class="header">Advanced Web Programming ~ Lynn Bailey ~ Section 5250</div><!-- end .header -->
+
+  <!-- Begin .header -->
+  <div class="header">Advanced Web Programming ~ Lynn Bailey ~ Section 5250</div>
+  <!-- End .header -->
+  
+  <!-- Begin .sidebar1 -->
   <div class="sidebar1">
     <ul class="nav">
       <li><a href="index.php">about me</a></li>
@@ -22,12 +28,15 @@
     <p class="checkers"><a href="http://validator.w3.org/check?uri=referer">Check HTML</a></p>
     <p class="checkers"><a href="http://jigsaw.w3.org/css-validator/check/referer">
     Check CSS</a></p>
-    <!-- end .sidebar1 --></div>
+    <!-- End .sidebar1 --></div>
+    
+  <!-- Begin .content -->  
   <div class="content">
     <h1>Just the facts, friends!</h1>
     <p>I&rsquo;m a retired school administrator who just can&rsquo;t seem to get the retirement part right. I&rsquo;m working a 40 hour bridge job, serving on several boards and committees, managing a senior community, and taking SRJC online courses. </p>
     <p>So far I&rsquo;ve taken Photoshop I and II, HTML/CSS A &amp; B, and Dreamweaver I. I&rsquo;m currently enrolled for Adv Web Programming and Flash Animation. I&rsquo;m sure I&rsquo;ll keep taking the Adobe classes until I run out of online classes to take. I live over on the coast, so the online option is excellent for me.</p>
-    
+
+  <!-- Begin PHP -->
 <?php
 	$centennial = '2046-04-19';
 					
@@ -66,15 +75,22 @@
 	$seconds = seconds ($centennial) ;
 	$seconds = intval ($seconds) ;
 ?>
+  <!-- End PHP -->
 
-      
     <p>I won&rsquo;t reveal my age directly, but will tell you that it will be <span class="boldface"><?php echo $years ?></span> years, <span class="boldface"><?php echo $days ?></span> day(s), <span class="boldface"><?php echo $hours ?></span> hour(s), <span class="boldface"><?php echo $minutes ?></span> minute(s), and <span class="boldface"><?php echo $seconds ?></span> second(s) until I celebrate my personal centennial. You are invited to the party!<!-- end .content --></p>
     <p>Refresh this page to verify that the seconds actually do count down!</p>
   </div>
+  <!-- End .content -->
+  
+  <!-- Begin .footer -->
   <div class="footer">
     <p>Lynn Bailey ~ Advanced Web Programming~ Section 5250 ~ Spring 2013</p>
     <p><a href="mailto:lbailey2@aol.com">lbailey2@aol.com</a></p>
-    <!-- end .footer --></div>
-<!-- end .container --></div>
+  <!-- End .footer --></div>
+    
+  </div>
+  <!-- End .container -->
+
 </body>
+
 </html>
